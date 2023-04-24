1857f2abadf68d9d8940e32973a39c365017aa19
bezbak <Cemetey.2019@gmail.com>
2023-04-24 16:49:40 +0600

    'deleteass-Student'

--- videos/models.py
+++ videos/models.py
@@ -25,19 +25,3 @@ class Videos(models.Model):
     class Meta:
         verbose_name = 'Видео'
         verbose_name_plural = 'Видео'
-
-class Student(models.Model):
-    full_name = models.CharField(
-        max_length=255
-    )
-    age = models.SmallIntegerField()
-    photo = models.ImageField(
-        upload_to='students_images/'
-    )
-    
-    def __str__(self):
-        return self.full_name
-    
-    class Meta:
-        verbose_name = 'Студент'
-        verbose_name_plural = 'Студенты'
